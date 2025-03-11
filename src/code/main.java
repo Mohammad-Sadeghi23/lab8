374ffbf87b5198f221700488dec35dd56aed15ac
Mohammad Sadeghi <mohammad2184@gmail.com>
2025-03-10 21:12:14 -0700

    first commit

---
+++ src/code/main.java
@@ -0,0 +1,15 @@
+package code;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class main
+{
+    public static void main(final String[] args) throws IOException
+    {
+
+    }
+
+}
