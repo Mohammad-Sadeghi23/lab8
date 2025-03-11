cb8ae8e09acca1a402b312f9155f0a39f7eda123
Mohammad Sadeghi <mohammad2184@gmail.com>
2025-03-10 21:16:19 -0700

    created package

--- src/code/ca/bcit/comp2522/lab6/main.java
+++ src/code/ca/bcit/comp2522/lab6/main.java
@@ -1,9 +1,6 @@
-package code;
+package ca.bcit.comp2522.lab6;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class main
 {
